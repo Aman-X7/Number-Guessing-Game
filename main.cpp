7c19f579fa701ddc81421fdcd0ee96a98c3f4b13
Aman Raj <86025379+Aman-X7@users.noreply.github.com>
2024-06-25 12:48:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+
+   int randomnumber = rand() % 100 + 1; // create a random number in range 1-100
+   cout << "Welcome to guess a number game.guess any number from 1 to 100!" << endl;
+   int trial = 1;
+   int guess = 0;
+
+
+   while (guess != randomnumber){
+        cout << "Guess! ";
+        cin >> guess; //taking input for the number guessed by the user
+            if (guess < randomnumber) {
+                cout << "oops! - too low. Try again." << endl;
+            }else if (guess > randomnumber) {
+                cout << "Too high. Try again. "  << endl;
+            }
+        trial ++;
+        }
+        
+        string comment;
+        if (trial < 10) {
+            comment = " - you are a superstar!";
+        } else if (trial > 30) {
+            comment = "Uhm.. That took you a long time.";
+        }
+        cout << "Great job you  took " << trial << "  total attempts!" << comment;
+
+    return 0;
+}
